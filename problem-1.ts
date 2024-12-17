12ae164a885bfe5ccdb793202f29c7b1ebc4848f
hossain <hossainahamedk@gmail.com>
2024-12-18 01:20:05 +0600

    created the files and started the problem solving

---
+++ problem-1.ts
@@ -0,0 +1,3 @@
+// Write a TypeScript function sumArray that takes an array of numbers and returns the sum of all elements in the array. 
+
+
---
+++ problem-2.ts
@@ -0,0 +1,3 @@
+// Create a TypeScript function removeDuplicates that accepts an array of numbers and returns a new array with duplicates removed. Preserve the original order of elements.
+
+
